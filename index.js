a95dcc4c1dde31409eb1845f00f1656f63e0f74d
Hiago 7k <hiago7k@gmail.com>
2026-05-05 18:16:47 -0300

    function x arrow function

--- index.js
+++ index.js
@@ -7,7 +7,19 @@
 // {
 //     console.log(`A soma dos dois numeros é ${somar}`)
 // }
-
+// ma pratica
 // Soma(numeroA + numeroB)
 
+// function Soma(numeroA, numeroB)
+// {
+//     return numeroA + numeroB;
+// }
+// console.log(`O resultado da soma é ${Soma(2, 2)}`);
+
+const soma = (a,b) => 
+    {
+        return a + b;
+    }   
+    
+    console.log(`A soma e ${soma(10, 100)}`);
 
