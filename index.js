b6908e39060692593ddc4a51a9ad27f48db62096
Hiago 7k <hiago7k@gmail.com>
2026-05-05 18:05:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// let numeroA = 10;
+// let numeroB = 6;
+// let somar = numeroA + numeroB;
+
+// function Soma(numeroA, numeroB)
+// {
+//     console.log(`A soma dos dois numeros é ${somar}`)
+// }
+
+// Soma(numeroA + numeroB)
+
+
